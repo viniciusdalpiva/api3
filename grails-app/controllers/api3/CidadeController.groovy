fd6a526ff5926244e3266760aca98d14c845c74f
viniciusdalpiva <viniciusdalpivadasilva@gmail.com>
2023-09-10 22:22:13 -0300

    fix: removidas funcionalidades desnecessárias nao funcionais

--- grails-app/controllers/api3/CidadeController.groovy
+++ grails-app/controllers/api3/CidadeController.groovy
@@ -9,60 +9,36 @@ class  CidadeController {
 
 
     def list() {
-        try {
-            def api2BaseUrl = "http://localhost:8080/api2/"
-            def client = new RESTClient(api2BaseUrl)
-            def response = client.get(path: "cidade/list")
-
-            if (response.status == 200) {
-                def jsonResponse = new JsonBuilder()
-                jsonResponse {
-                    data response.data
-                }
-                render(contentType: 'application/json', text: jsonResponse.toString())
-
-                // Chamada correta para salvar o log
-                def retorno = LogService.salvarLog("Descrição do log")
-
-            } else {
-                render(status: response.status, contentType: 'application/json') {
-                    error: "Erro ao buscar a cidade na API2."
-                }
-            }
-        } catch (Exception e) {
-            render(status: 500, contentType: 'application/json') {
-                error: "Erro ao processar a requisição."
-            }
+        def api2BaseUrl = "http://localhost:8080/api2/"
+        def client = new RESTClient(api2BaseUrl)
+        def response = client.get(path: "cidade/list")
+        def jsonResponse = new JsonBuilder()
+        jsonResponse {
+            data response.data
         }
+        render(contentType: 'application/json', text: jsonResponse.toString())
     }
 
     def get(Long id) {
-        try {
-            def api2BaseUrl = "http://localhost:8080/api2/"
-            def client = new RESTClient(api2BaseUrl)
-            def response = client.get(path: "cidade/get/$id")
-
-            if (response.status == 200) {
-                def jsonResponse = new JsonBuilder()
-                jsonResponse {
-                    data response.data
-                }
-                render(contentType: 'application/json', text: jsonResponse.toString())
-
-                // Chamada correta para salvar o log
-                def retorno = LogService.salvarLog("Descrição do log")
-
-            } else {
-                render(status: response.status, contentType: 'application/json') {
-                    error: "Erro ao buscar a cidade na API2."
-                }
-            }
-        } catch (Exception e) {
-            render(status: 500, contentType: 'application/json') {
-                error: "Erro ao processar a requisição."
-            }
+        def api2BaseUrl = "http://localhost:8080/api2/"
+        def client = new RESTClient(api2BaseUrl)
+        def response = client.get(path: "cidade/get/$id")
+        def jsonResponse = new JsonBuilder()
+        jsonResponse {
+            data response.data
         }
+        render(contentType: 'application/json', text: jsonResponse.toString())
     }
 
+    def save() {
+        def api2BaseUrl = "http://localhost:8080/api2/"
+        def client = new RESTClient(api2BaseUrl)
+        def response = client.get(path: "cidade/save")
+        def jsonResponse = new JsonBuilder()
+        jsonResponse {
+            data response.data
+        }
+        render(contentType: 'application/json', text: jsonResponse.toString())
+    }
 
 }
