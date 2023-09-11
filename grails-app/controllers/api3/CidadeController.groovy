b1c25ff7905df59f4bf75f4797db9f5b672a99f3
viniciusdalpiva <viniciusdalpivadasilva@gmail.com>
2023-09-10 22:10:26 -0300

    Cidade controller
    get e list funcionais

--- grails-app/controllers/api3/CidadeController.groovy
+++ grails-app/controllers/api3/CidadeController.groovy
@@ -7,11 +7,12 @@ class  CidadeController {
     static responseFormats = ["json"]
     def LogService
 
-    def show(Long id) {
+
+    def list() {
         try {
             def api2BaseUrl = "http://localhost:8080/api2/"
             def client = new RESTClient(api2BaseUrl)
-            def response = client.get(path: "cidade/show/$id")
+            def response = client.get(path: "cidade/list")
 
             if (response.status == 200) {
                 def jsonResponse = new JsonBuilder()
@@ -34,4 +35,34 @@ class  CidadeController {
             }
         }
     }
+
+    def get(Long id) {
+        try {
+            def api2BaseUrl = "http://localhost:8080/api2/"
+            def client = new RESTClient(api2BaseUrl)
+            def response = client.get(path: "cidade/get/$id")
+
+            if (response.status == 200) {
+                def jsonResponse = new JsonBuilder()
+                jsonResponse {
+                    data response.data
+                }
+                render(contentType: 'application/json', text: jsonResponse.toString())
+
+                // Chamada correta para salvar o log
+                def retorno = LogService.salvarLog("Descrição do log")
+
+            } else {
+                render(status: response.status, contentType: 'application/json') {
+                    error: "Erro ao buscar a cidade na API2."
+                }
+            }
+        } catch (Exception e) {
+            render(status: 500, contentType: 'application/json') {
+                error: "Erro ao processar a requisição."
+            }
+        }
+    }
+
+
 }
