6bcba2a4ceaa9df64bc3a5cdacbb9ec5dd948753
viniciusdalpiva <viniciusdalpivadasilva@gmail.com>
2023-09-12 22:21:06 -0300

    Criado funcionarioController

---
+++ grails-app/controllers/api3/FuncionarioController.groovy
@@ -0,0 +1,109 @@
+package api3
+
+import groovy.json.JsonBuilder
+import groovyx.net.http.RESTClient
+
+class FuncionarioController {
+    static responseFormats = ["json"]
+    static defaultAction = "get"
+
+    def list() {
+        def api2BaseUrl = "http://localhost:8080/api2/"
+        def client = new RESTClient(api2BaseUrl)
+        def response = client.get(path: "funcionario/list")
+        def jsonResponse = new JsonBuilder()
+        jsonResponse {
+            data response.data
+        }
+        render(contentType: 'application/json', text: jsonResponse.toString())
+    }
+
+    def get(Long id) {
+        def api2BaseUrl = "http://localhost:8080/api2/"
+        def client = new RESTClient(api2BaseUrl)
+        def response = client.get(path: "funcionario/get/$id")
+        def jsonResponse = new JsonBuilder()
+        jsonResponse {
+            data response.data
+        }
+        render(contentType: 'application/json', text: jsonResponse.toString())
+    }
+
+    def save() {
+        if (request.method != "POST") {
+            response.setStatus(405)
+            render "Método não permitido para esta ação."
+            return
+        }
+        try {
+            def funcionarioData = request.JSON
+            def api2BaseUrl = "http://localhost:8080/api2/"
+            def client = new RESTClient(api2BaseUrl)
+            def response = client.post(
+                    path: "funcionario/save",
+                    requestContentType: 'application/json',
+                    body: funcionarioData
+            )
+            def jsonResponse = new JsonBuilder()
+            jsonResponse {
+                data response.data
+            }
+            render(contentType: 'application/json', text: jsonResponse.toString())
+        } catch (Exception e) {
+            renderError(e.message)
+        }
+    }
+
+    def update(Long id) {
+        if (request.method != "PUT") {
+            response.setStatus(405)
+            render "Método não permitido para esta ação."
+            return
+        }
+        try {
+            def funcionarioData = request.JSON
+            def api2BaseUrl = "http://localhost:8080/api2/"
+            def client = new RESTClient(api2BaseUrl)
+            def response = client.put(
+                    path: "funcionario/update/$id",
+                    requestContentType: 'application/json',
+                    body: funcionarioData
+            )
+            def jsonResponse = new JsonBuilder()
+            jsonResponse {
+                data response.data
+            }
+            render(contentType: 'application/json', text: jsonResponse.toString())
+        } catch (Exception e) {
+            renderError(e.message)
+        }
+    }
+
+    def delete(Long id) {
+        if (request.method != "DELETE") {
+            response.setStatus(405) // Método não permitido
+            render "Método não permitido para esta ação."
+            return
+        }
+        try {
+            def api2BaseUrl = "http://localhost:8080/api2/"
+            def client = new RESTClient(api2BaseUrl)
+            def response = client.delete(
+                    path: "funcionario/delete/$id"
+            )
+            if (response.status == 204) {
+                def jsonResponse = new JsonBuilder()
+                jsonResponse {
+                    message "Funcionário ${id} deletado com sucesso!"
+                }
+                render(contentType: 'application/json', status: 200, text: jsonResponse.toString())
+            }
+        } catch (Exception e) {
+            def jsonResponse = new JsonBuilder()
+            jsonResponse {
+                message "Não é possível excluir o funcionário."
+            }
+            render(contentType: 'application/json', status: 200, text: jsonResponse.toString())
+        }
+    }
+}
---
+++ src/test/groovy/api3/FuncionarioControllerSpec.groovy
@@ -0,0 +1,18 @@
+package api3
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class FuncionarioControllerSpec extends Specification implements ControllerUnitTest<FuncionarioController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+        true == false
+    }
+}
